cf7e4fd929d05dae0c48e9fdd2375bdbde643ab4
RngAd33 <yxbsyx@outlook.com>
2025-07-01 00:31:03 +0800

    move

---
+++ src/test/java/com/yupi/web/MainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yupi.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 测试类
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
